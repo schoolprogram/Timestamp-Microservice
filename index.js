923c6986e56e7d9f95a98fa01a042e28ad0d210b
schoolprogram <47223485+schoolprogram@users.noreply.github.com>
2024-06-18 13:13:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/:date", function(req, res) {
+  const regex = new RegExp('^[0-9]+$');
+  let dateParam = req.params.date;
+  if(regex.test(req.params.date)) {
+    dateParam = parseInt(dateParam);
+  }
+
+  let date = new Date(dateParam);
+
+  if(isNaN(date.valueOf())) {
+    res.json({error: "Invalid Date"})
+  } else {
+    res.json({unix: date.valueOf(), utc: date.toUTCString()})
+  }
+})
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
